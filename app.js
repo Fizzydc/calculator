eb63d681fde97d267c4f6e27d7ddd04e08043b9d
faruq <muideenfaruq709@gmail.com>
2024-04-27 21:46:55 +0100

    new line of code

---
+++ app.js
@@ -0,0 +1,21 @@
+let display = document.getElementById("display");
+let currentInput = "";
+
+function appendValue(value) {
+  currentInput += value;
+  display.value = currentInput;
+}
+
+function calculate() {
+  try {
+    currentInput = eval(currentInput);
+    display.value = currentInput;
+  } catch (e) {
+    display.value = "Error";
+  }
+}
+
+function clearDisplay() {
+  currentInput = "";
+  display.value = "";
+}
